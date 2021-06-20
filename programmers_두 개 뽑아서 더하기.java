a144afcbb10bc8f02f4ece20e18c0110fcba53d8
qowldud95 <girlwldud@naver.com>
2021-06-20 17:13:16 +0900

    20210620_algorithm

--- programmers_두 개 뽑아서 더하기.java
+++ programmers_두 개 뽑아서 더하기.java
@@ -6,34 +6,33 @@ import java.util.Collections;
 
 public class Main {
 	public static void main(String[] args) {
-		int numbers[] = {2,1,3,4,1};    //주어진 정수배열 numbers
-	
-		ArrayList <Integer> al = new ArrayList<>();
-		
-		for(int i = 0; i < numbers.length; i++) {
-			for(int j = i+1; j < numbers.length; j++) {
+		int numbers[] = { 2, 1, 3, 4, 1 }; // 주어진 정수배열 numbers
+
+		ArrayList<Integer> al = new ArrayList<>();
+
+		for (int i = 0; i < numbers.length; i++) {
+			for (int j = i + 1; j < numbers.length; j++) {
 				int result = numbers[i] + numbers[j];
-				if(al.contains(result)) {
+				if (al.contains(result)) {
 					continue;
 				}
 				al.add(numbers[i] + numbers[j]);
 			}
 		}
-		
-		//Collections.sort(al);
-		
-		
-		//ArrayList -> Array 로 변경 (프로그래머스 조건에 맞춰서 변경한것임)
+
+		// Collections.sort(al);
+
+		// ArrayList -> Array 로 변경 (프로그래머스 조건에 맞춰서 변경한것임)
 		int[] answer = new int[al.size()];
-		int size=0;
-		for(int temp : al) {
-			answer[size++]=temp;
+		int size = 0;
+		for (int temp : al) {
+			answer[size++] = temp;
 		}
 		Arrays.sort(answer);
 
-		for(int i=0; i<answer.length; i++) {
+		for (int i = 0; i < answer.length; i++) {
 			System.out.print(answer[i]);
 		}
-		
+
 	}
 }
