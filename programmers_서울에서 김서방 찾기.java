f586b1f89af33c80122102ca31290f304b6bc70d
qowldud95 <girlwldud@naver.com>
2021-06-20 14:48:59 +0900

    20210620_algorithm

--- programmers_서울에서 김서방 찾기.java
+++ programmers_서울에서 김서방 찾기.java
@@ -2,17 +2,18 @@ package test;
 
 public class Main {
 	public static void main(String[] args) {
-		
-		String[] seoul = {"Jin","Kim"};
-		
+
+		String[] seoul = { "Jin", "Kim" };
+
 		String answer = "";
-		
+
 		for (int i = 0; i < seoul.length; i++) {
 			if (seoul[i].equals("Kim")) {
 				answer = "김서방은 " + i + "에 있다";
 			}
 		}
+
 		System.out.println(answer);
-		
+
 	}
 }
